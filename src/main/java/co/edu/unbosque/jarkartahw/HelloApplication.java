329d8145caf3aaa4e62b0f3cfbae45249634dad1
JorgeGarcia855 <jlgarciap@unbosque.edu.co>
2021-09-21 16:58:56 -0500

    aa

---
+++ src/main/java/co/edu/unbosque/jarkartahw/HelloApplication.java
@@ -0,0 +1,9 @@
+package co.edu.unbosque.jarkartahw;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
